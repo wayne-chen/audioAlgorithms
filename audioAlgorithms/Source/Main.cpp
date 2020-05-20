1987b24811a5feb0b19c0008c093b5e708f125ac
Chen, Wayne <wayne.chen@dolby.com>
2020-05-19 21:00:47 -0700

    moving code

--- audioAlgorithms/Source/Main.cpp
+++ audioAlgorithms/Source/Main.cpp
@@ -9,13 +9,33 @@
 */
 
 #include <JuceHeader.h>
+#include "muLaw.h"
 
 //==============================================================================
 int main (int argc, char* argv[])
 {
 
     // ..your code goes here!
+    int numChannels = 5, numSamples = 50;
+    MultiBuffer multiBuffer;
+    std::srand(std::time(nullptr)); // use current time as seed for random generator
+    float freq = 1000.0f, fs = 48000.0f;
 
+    float val;
+    for (int c = 0; c < numChannels; c++)
+    {
+        //val = (std::rand() * 2.0f / RAND_MAX) - 1.0f;
+        multiBuffer.push_back(std::vector<float>(numSamples));
+        for (int i = 0; i < numSamples; i++)
+        {
+            multiBuffer[c][i] = std::sin(2.0f * freq * pi * i / fs);
+        }
+    }
+    printBuffer(multiBuffer);
+
+    mu_compand(&multiBuffer);
+
+    printBuffer(multiBuffer);
 
     return 0;
 }
--- audioAlgorithms/Source/muLaw.h
+++ audioAlgorithms/Source/muLaw.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <cmath>
 #include <ctime>
 #include <cstdlib>
@@ -66,31 +68,4 @@ void mu_decode(float** buf, int numChannels, int numSamples)
                 / std::log(1.0f + mu);  // denominator
         }
     }
-}
-
-int main()
-{
-    int numChannels = 5, numSamples = 50;
-    MultiBuffer multiBuffer;
-    std::srand(std::time(nullptr)); // use current time as seed for random generator
-    float freq = 1000.0f, fs = 48000.0f;
-
-    float val;
-    for (int c = 0; c < numChannels; c++)
-    {
-        //val = (std::rand() * 2.0f / RAND_MAX) - 1.0f;
-        multiBuffer.push_back(std::vector<float>(numSamples));
-        for (int i = 0; i < numSamples; i++)
-        {
-            multiBuffer[c][i] = std::sin(2.0f * freq * pi * i / fs);
-        }
-    }
-    printBuffer(multiBuffer);
-
-    mu_compand(&multiBuffer);
-
-    printBuffer(multiBuffer);
-
-
-    return 0;
 }
