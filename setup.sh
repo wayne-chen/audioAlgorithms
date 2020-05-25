1f54947dc206e3f2b3b40bd91b8288547efd6b65
Wayne Chen <chen.wayne@outlook.com>
2020-05-25 16:32:38 -0700

    fixing script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/sh
 
 cd FRUT
-mkdir build && cd build/
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+cd build/
 cmake .. -DCMAKE_INSTALL_PREFIX=../prefix -DJUCE_ROOT=../../JUCE
 cmake --build . --target install
 
 # build project
 cd ../../audioAlgorithms
 ../FRUT/prefix/FRUT/bin/Jucer2Reprojucer audioAlgorithms.jucer ../FRUT/prefix/FRUT/cmake/Reprojucer.cmake
-mkdir build && cd build/
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+cd build/
 cmake .. -GXcode
 cmake --build .
